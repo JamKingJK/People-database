bc91e9f564c2dd727ffd2e2989b613db55b6e344
Kacper <kacper.pabianiak1@gmail.com>
2021-05-20 19:33:21 +0200

    Added to git

---
+++ People-database/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace People_database
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PersonalId { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Sex { get; set; }
+
+    }
+    public static class PersonExtensions
+    {
+        public static Person AddPerson(this Person p)
+        {
+            Console.Write("Enter first name: ");
+            string fn = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string ln = Console.ReadLine();
+            Console.Write("Enter personal ID: ");
+            string id = Console.ReadLine();
+            Console.Write("Enter date of birth: ");
+            string date = Console.ReadLine();
+            Console.Write("Enter sex: ");
+            string sex = Console.ReadLine();
+            Person person = new()
+            {
+                FirstName = fn,
+                LastName = ln,
+                PersonalId = id,
+                DateOfBirth = date,
+                Sex = sex,
+            };
+            return person;
+        }
+    }
+
+
+    internal static class Program
+    {
+        private static void StartupPrep()
+        {
+            //Console.Write(File.ReadAllText(path));
+            //var jsontxt = File.ReadAllText(path);
+            //JsonSerializerOptions options = new() {WriteIndented = true};
+            //string jsonString = JsonSerializer.Serialize(testlist, options);
+            //File.WriteAllText(path, jsonString);
+        }
+        private static void CommandListener()
+        {
+            const string path = @"C:\Users\Kacper\RiderProjects\ConsoleApp1\ConsoleApp1\database.json"; //temp path
+            string jsontxt = File.ReadAllText(path);
+            
+            // TODO Add deserializing json file to a list
+            
+            List<Person> people = new List<Person>();
+            while(true)
+            {
+                string command = Console.ReadLine();
+                String[] strlist = command.Split(" ");
+                if (strlist[0] == "help")
+                {
+                    if (strlist.Length > 1)
+                    {
+                        Console.WriteLine("Error: Too many arguments");
+                    }
+                    else
+                    {
+                        Console.Write("\nUsage: [add]\n");
+                    }
+                }
+                else if (strlist[0] == "add")
+                {
+                    if (strlist.Length > 1)
+                    {
+                        Console.WriteLine("Error: Too many arguments");
+                    }
+                    else
+                    {
+                        Person obj = new();
+                        people.Add(obj.AddPerson());
+                    }
+                }
+                else if (strlist[0] == "exit")
+                {
+                    break;
+                }
+            }
+        }
+        private static void Main(string[] args)
+        {
+            CommandListener();
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-People-database
+﻿# People database
+
+This application lets you keep a persistent database of people and their personal information.
+You can view the list of people in the database, as well as add, modify and delete particular entries.
+The database lets you store the following information:
+* First Name
+* Last Name
+* Personal ID
+* Date of Birth
+* Sex
