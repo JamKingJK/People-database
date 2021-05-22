802554a262d38e24e96322039a0087abeaec39a0
Kacper <kacper.pabianiak1@gmail.com>
2021-05-22 20:33:42 +0200

    Add all necessary functionality

--- People-database/Program.cs
+++ People-database/Program.cs
@@ -8,6 +8,7 @@ namespace People_database
 {
     public class Person
     {
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PersonalId { get; set; }
@@ -19,25 +20,63 @@ namespace People_database
     {
         public static Person AddPerson(this Person p)
         {
+            string id = Guid.NewGuid().ToString();
             Console.Write("Enter first name: ");
             string fn = Console.ReadLine();
             Console.Write("Enter last name: ");
             string ln = Console.ReadLine();
             Console.Write("Enter personal ID: ");
-            string id = Console.ReadLine();
+            string personalId = Console.ReadLine();
             Console.Write("Enter date of birth: ");
             string date = Console.ReadLine();
             Console.Write("Enter sex: ");
             string sex = Console.ReadLine();
             Person person = new()
             {
+                Id = id,
                 FirstName = fn,
                 LastName = ln,
-                PersonalId = id,
+                PersonalId = personalId,
                 DateOfBirth = date,
                 Sex = sex,
             };
             return person;
+        }
+
+        public static Person SetFirstName(this Person p)
+        {
+            Console.Write("Enter new first name: ");
+            string fn = Console.ReadLine();
+            p.FirstName = fn;
+            return p;
+        }
+        public static Person SetLastName(this Person p)
+        {
+            Console.Write("Enter new last name: ");
+            string ln = Console.ReadLine();
+            p.LastName = ln;
+            return p;
+        }
+        public static Person SetPersonalId(this Person p)
+        {
+            Console.Write("Enter new personal ID: ");
+            string id = Console.ReadLine();
+            p.PersonalId = id;
+            return p;
+        }
+        public static Person SetDateOfBirth(this Person p)
+        {
+            Console.Write("Enter new date of birth: ");
+            string dob = Console.ReadLine();
+            p.DateOfBirth = dob;
+            return p;
+        }
+        public static Person SetSex(this Person p)
+        {
+            Console.Write("Enter new sex: ");
+            string sex = Console.ReadLine();
+            p.Sex = sex;
+            return p;
         }
     }
     internal static class Program
@@ -52,7 +91,7 @@ namespace People_database
                 Console.WriteLine("Database location: " + fullpath);
             } 
             List<Person> people = new List<Person>();
-            if( new FileInfo(fullpath).Length != 0 )
+            if (new FileInfo(fullpath).Length != 0)
             {
                 var jsontxt = File.ReadAllText(fullpath);
                 var existingPeople = JsonSerializer.Deserialize<List<Person>>(jsontxt);
@@ -60,6 +99,7 @@ namespace People_database
             }
             while(true)
             {
+                Console.Write("\n>");
                 string command = Console.ReadLine();
                 if (command == null) continue;
                 string[] strlist = command.Split(" ");
@@ -71,7 +111,13 @@ namespace People_database
                     }
                     else
                     {
-                        Console.Write("\nUsage: [add]\n");
+                        Console.WriteLine("\nAvailable commands:");
+                        Console.WriteLine("add          Adds a new entry into the database");
+                        Console.WriteLine("edit         Modifies an entry by given ID");
+                        Console.WriteLine("delete       Removes an entry by given ID");
+                        Console.WriteLine("list         Lists all entries in currently in the database");
+                        Console.WriteLine("commit       Finalizes all changes and applies them to the database");
+                        Console.WriteLine("discard      Discards all pending changes to the database");
                     }
                 }
                 else if (strlist[0] == "add")
@@ -86,26 +132,92 @@ namespace People_database
                         people.Add(obj.AddPerson());
                     }
                 }
-                else if (strlist[0] == "list") // TODO Fix Unhandled exception when given no arguments
+                else if (strlist[0] == "list")
                 {
-                    if (strlist.Length > 2)
+                    if (strlist.Length > 1)
                     {
+                        
                         Console.WriteLine("Error: Too many arguments");
-                    }
-                    else if (strlist.Length <= 0)
-                    {
-                        Console.WriteLine("Error: Too few arguments");
                     }
                     else
                     {
-                        switch (strlist[1])
+                        var jsontxt = File.ReadAllText(fullpath);
+                        var existingPeople = JsonSerializer.Deserialize<List<Person>>(jsontxt);
+                        foreach (var person in existingPeople)
                         {
-                            case "all":
-                                Console.Write(people.ToArray()); //TODO Make it list all values of the objects 
-                                break;
-                            case "existing":
-                                break;
+                            Console.WriteLine("\nID: " + person.Id);
+                            Console.WriteLine("First name: " + person.FirstName);
+                            Console.WriteLine("Last name: " + person.LastName);
+                            Console.WriteLine("Personal ID: " + person.PersonalId);
+                            Console.WriteLine("Date of birth: " + person.DateOfBirth);
+                            Console.WriteLine("Sex: " + person.Sex);
                         }
+                    }
+                }
+                else if (strlist[0] == "edit")
+                {
+                    if (strlist.Length == 2)
+                    {
+                        string guid = strlist[1];
+                        var tempPerson = people.Find(person => person.Id == guid);
+                        if (tempPerson == null)
+                        {
+                            Console.WriteLine("Error: Incorrect ID");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Which attribute do you want to edit?");
+                            Console.WriteLine("First name (fname)");
+                            Console.WriteLine("Last name (lname)");
+                            Console.WriteLine("Personal ID (pid)");
+                            Console.WriteLine("Date of birth (dob)");
+                            Console.WriteLine("Sex (sex)");
+                            Console.Write(">");
+                            string choice = Console.ReadLine().ToUpper();
+                            switch (choice)
+                            {
+                                case "FNAME":
+                                    tempPerson.SetFirstName();
+                                    break;
+                                case "LNAME":
+                                    tempPerson.SetLastName();
+                                    break;
+                                case "PID":
+                                    tempPerson.SetPersonalId();
+                                    break;
+                                case "DOB":
+                                    tempPerson.SetDateOfBirth();
+                                    break;
+                                case "SEX":
+                                    tempPerson.SetSex();
+                                    break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: ID not specified or too many arguments passed");
+                    }
+                }
+                else if (strlist[0] == "delete")
+                {
+                    if (strlist.Length == 2)
+                    {
+                        string guid = strlist[1];
+                        var deletee = people.Find(person => person.Id == guid);
+                        if (deletee == null)
+                        {
+                            Console.WriteLine("Error: Incorrect ID");
+                        }
+                        else
+                        {
+                            people.Remove(deletee);
+                            Console.WriteLine($"Entry with ID {deletee.Id} was removed.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: ID not specified or too many arguments passed");
                     }
                 }
                 else if (strlist[0] == "commit")
@@ -113,6 +225,13 @@ namespace People_database
                     JsonSerializerOptions options = new() {WriteIndented = true};
                     string jsonString = JsonSerializer.Serialize(people, options);
                     File.WriteAllText(fullpath, jsonString);
+                }
+                else if (strlist[0] == "discard")
+                {
+                    people.Clear();
+                    var jsontxt = File.ReadAllText(fullpath);
+                    var existingPeople = JsonSerializer.Deserialize<List<Person>>(jsontxt);
+                    people.AddRange(existingPeople);
                 }
                 else if (strlist[0] == "exit")
                 {
